25cbd607cf0131161eb1a7591259abd67acfb800
Anthony sayegh <Sayeghanthony89012@gmail.com>
2022-01-10 12:14:25 +0200

    calendar

--- calendar/src/components/calendar.js
+++ calendar/src/components/calendar.js
@@ -2,7 +2,10 @@ import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import timeGridPlugin from '@fullcalendar/timegrid';
 import interactionPlugin from '@fullcalendar/interaction';
+import { Calendar } from '@fullcalendar/core'
 import "./Calendar.css";
+
+
 export default function FullCalendarApp() {
 
   
@@ -13,45 +16,86 @@ export default function FullCalendarApp() {
           title: 'event 1',
           start: '2022-01-06T10:00:00',
           end: '2022-01-06T12:00:00',
-          color: 'cyan'
+          color: '#CECDF2'
         },
         {
           id: 2,
           title: 'event 2',
-          start: '2021-06-16T13:00:00',
-          end: '2021-06-16T18:00:00',
-          color: 'red'
+          start: '2022-01-06T10:00:00',
+          end: '2022-01-06T12:00:00',
+          color: '#D4E8D7'
         },
         { 
           id: 3, 
           title: 'event 3', 
-          start: '2021-06-17', 
-          end: '2021-06-20' ,
-          color: 'green'
+          start: '2022-01-06T09:00:00', 
+          end: '2022-01-06T10:00:00' ,
+          color: '#F7E4F3'
         },
           
         
       ];
+      
+      
     return (
       <div className="App">
         <FullCalendar
+        
+          views={{
+            day: {
+                titleFormat: { month: 'short',day:'2-digit'  },
+                
+                dayHeaderFormat:{month:'long'  ,weekday: 'short',day:'2-digit' , omitCommas: true}
+                ,
+
+            },
+             month:{
+              titleFormat: {  month: 'long' },
+             },
+             week:{
+              titleFormat: { day:'2-digit', month: 'short' },
+              dayHeaderFormat:{ weekday: 'short', day:'2-digit', month:'short', omitCommas: true}
+             },       
+             timeGrid:  {
+              slotMinTime: '08:00',
+              slotMaxTime: '21:00'
+          }
+           }}
+           
           plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
           initialView="dayGridMonth"
+
           customButtons={{
             search_appointment: {
               text: 'SEARCH APPOINTMENTS',
               click: () => console.log('search apointments working'),
             },  
              date: {
-                text: 'title',
+                text:"fullcalendar",
                 click: () => console.log('search apointments working'),
               },
           }}
           headerToolbar = {{
             start: 'search_appointment',
-            center: 'prev,date,next',   
+            center: 'prev,title,next',   
             end: 'timeGridDay,timeGridWeek,dayGridMonth'
           }}
+          
+          businessHours = {{
+
+            startTime: '00:00', // a start time (10am in this example)
+            endTime: '21:00',
+            daysOfWeek: [ 1,2,3,4,5 ],
+
+          }}
+
+          scrollTime = {{
+            default: '06:00:00'
+          }}
+         
+                     
+
+          
           events={events}
           eventColor={events.color}
         />
